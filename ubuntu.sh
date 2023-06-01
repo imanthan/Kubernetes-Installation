1b66b06c882a938520d2160bd6e35629439a90e9
Manthan Sarage <55378851+imanthan@users.noreply.github.com>
2023-06-01 09:14:00 +0530

    Create ubuntu.sh
    kubernetes installation in ubuntu system.

---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+#manthan_script
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh 
+sudo systemctl restart docker
+sudo systemctl enable docker
+sudo vi /etc/containerd/config.toml 
+sudo systemctl restart docker
+sudo systemctl restart containerd
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
