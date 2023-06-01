c9386b9af49602877657f40b373eb0dc4e8c2414
Manthan Sarage <55378851+imanthan@users.noreply.github.com>
2023-06-01 09:12:55 +0530

    Centos.sh
    kubernetes installation on centos system.

---
+++ Centos.sh
@@ -0,0 +1,40 @@
+#manthan_script
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+systemctl restart docker
+sudo  systemctl restart docker
+sudo systemctl enable docker
+sudo docker ps
+ sudo vi /etc/containerd/config.toml   
+ sudo systemctl restart docker
+ sudo systemctl restart containerd
+
+ cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+sudo yum install -y kubectl
+sudo yum install -y kubeadm
+sudo yum install -y kubelet
+
